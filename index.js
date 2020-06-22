2519ce4e5917cdf90d8e2f96a4c820c17aa06743
kaique oliveira santos <kaiquevicio@gmail.com>
2020-06-22 16:01:53 -0300

    chore

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello' })
-})
+  return res.json({ message: 'Hello' });
+});
 
 app.listen(3333);
